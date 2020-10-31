e858ca27d3c6d1a3c4b6341e2677b86fb3f218f3
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-31 16:54:35 +0530

    [Abhishek] Add . Added to the UC3 branch

--- MSTestForInvoice/UnitTest1.cs
+++ MSTestForInvoice/UnitTest1.cs
@@ -6,12 +6,14 @@ namespace MSTestForInvoice
     [TestClass]
     public class UnitTest1
     {
+        
         InvoiceGenerator invoice = null;
         [TestInitialize]
         public void SetUp()
         {
             invoice = new InvoiceGenerator(RideType.NORMAL);
         }
+        //TC 1
         [TestMethod]
         public void GivenDistanceAndTimeReturnsFare()
         {
@@ -21,13 +23,22 @@ namespace MSTestForInvoice
             double fareExpected = 50;
             Assert.AreEqual(fareExpected, fareActual);
         }
+        //TC 2
+        // [TestMethod]
+        //public void GivenMultipleRidesReturnsTotalFares()
+        //{
+        //    Ride[] rides = { new Ride(2.0, 30), new Ride(1.0, 40) };
+        //    InvoiceSummary actual = invoice.CalculateFare(rides);
+        //   InvoiceSummary expected = new InvoiceSummary(2, 50.0);
+        //    Assert.AreEqual(expected, actual);
+        //}
+        //TC 3
         [TestMethod]
-        public void GivenMultipleRidesReturnsTotalFares()
-        {
+        public void GivenMultipleRidesreturnsTotalFareAndAverageFare() {
             Ride[] rides = { new Ride(2.0, 30), new Ride(1.0, 40) };
-            InvoiceSummary actual = invoice.CalculateFare(rides);
-            InvoiceSummary expected = new InvoiceSummary(2, 50.0);
-            Assert.AreEqual(expected, actual);
+                InvoiceSummary actual = invoice.CalculateFare(rides);
+               InvoiceSummary expected = new InvoiceSummary(2, 50.0,25.0);
+               Assert.AreEqual(expected, actual);
         }
     }
 }
